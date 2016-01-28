5b3de188b63cd6dd35be9462b95340dee2a0d6fe
Daniel Rahier <daniel@rahier.biz>
2016-01-29 00:14:10 +0100

    Woops

--- src/CuteNodeWidget.h
+++ src/CuteNodeWidget.h
@@ -11,5 +11,6 @@ public:
     explicit CuteNodeWidget(QGraphicsItem* parent = nullptr, Qt::WindowFlags flags = 0);
     ~CuteNodeWidget();
 
+private:
     void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
 };
